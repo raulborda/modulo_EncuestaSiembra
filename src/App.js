fce038430cc2e25d7d495350c2e3a8ea649d130e
juliperelda <juliperelda@gmail.com>
2023-06-08 09:31:17 -0300

    Comenzando

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { ConfigProvider } from "antd";
+import esES from "antd/lib/locale/es_ES";
+import "./App.css";
+import { useState } from "react";
+import { GlobalContext } from "./components/context/GlobalContext";
+import { ViewGeneral } from "./components/ui/ViewGeneral";
 
 function App() {
+
+
+
+  //* Id de usuario que se obtiene desde local storage
+  const idUsu = localStorage.getItem("usuario");
+  // const [usu, setUsu] = useState(idUsu);
+  const [usu, setUsu] = useState(1);
+  //* Id de cliente que se obtine desde local storage
+  // const idC = localStorage.getItem("cliSelect");
+  // const idC = 2; // .153
+  // const idC = 2083; //.28
+  const idC = 2049; //.28
+  const [idCliente, setIdCliente] = useState(idC);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <GlobalContext.Provider
+      value={{
+        usu, 
+        setUsu,
+        idCliente,
+        setIdCliente,
+      }}
+    >
+      {/* <ApolloProvider client={client}> */}
+        <ConfigProvider
+          locale={esES}
+          theme={{
+            token: {
+              colorPrimary: "#56b43c",
+            },
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <ViewGeneral />
+        </ConfigProvider>
+      {/* </ApolloProvider> */}
+    </GlobalContext.Provider>
   );
 }
 

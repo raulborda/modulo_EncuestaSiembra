5e7fa7367dc546889c4ca293a0da5d4fc02fd33e
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2023-07-31 13:00:25 -0300

    se modifico variables dinamicas y se corrigio valores de carga de ciclo en el envio del formulario

--- src/components/ui/EncuestaSiembra.js
+++ src/components/ui/EncuestaSiembra.js
@@ -1405,8 +1405,8 @@ export const EncuestaSiembra = ({ cosechaActiva }) => {
           >
             <iframe
               loading="lazy"
-              src={`${URLDrawer}/tati/file_dos/?drawer=${drawerUpload}&modori_id=${modori}&filter_id=${filter}&usu_id=${usu}&generico_id=${generico}&cli_id=${cliEnc}`} // para usar local
-              //src={`${URLDrawer}/duoc/file_dos/?drawer=${drawerUpload}&modori_id=${modori}&filter_id=${filter}&usu_id=${usu}&generico_id=${generico}&cli_id=${cliEnc}`} // para usar con resto de crm
+              //src={`${URLDrawer}/tati/file_dos/?drawer=${drawerUpload}&modori_id=${modori}&filter_id=${filter}&usu_id=${usu}&generico_id=${generico}&cli_id=${cliEnc}`} // para usar local
+              src={`${URLDrawer}/duoc/file_dos/?drawer=${drawerUpload}&modori_id=${modori}&filter_id=${filter}&usu_id=${usu}&generico_id=${generico}&cli_id=${cliEnc}`} // para usar con resto de crm
               width={"100%"}
               height={"100%"}
               style={{ border: "none" }}
